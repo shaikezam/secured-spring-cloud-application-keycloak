2be2b462335def5ef3677c1438095c33b5e5f454
shaikezam <shaike.zam@gmail.com>
2023-05-25 00:25:43 +0300

    test

--- application/src/main/java/io/shaikezam/application/controller/APIController.java
+++ application/src/main/java/io/shaikezam/application/controller/APIController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("lol")
 public class APIController {
-
+ 
     @GetMapping
     public String lol() {
         return "WOW";
