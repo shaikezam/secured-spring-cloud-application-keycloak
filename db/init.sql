8020af26c2774ce255638a34c9b5da1c4e3169ee
shaikezam <shaike.zam@gmail.com>
2023-06-19 22:11:32 +0300

    add keycloak

--- db/init.sql
+++ db/init.sql
@@ -1,5 +1,7 @@
 create DATABASE products;
 create DATABASE orders;
+create DATABASE keycloak;
 
 GRANT ALL PRIVILEGES ON `products`.* TO 'admin'@'%';
 GRANT ALL PRIVILEGES ON `orders`.* TO 'admin'@'%';
+GRANT ALL PRIVILEGES ON `keycloak`.* TO 'admin'@'%';
