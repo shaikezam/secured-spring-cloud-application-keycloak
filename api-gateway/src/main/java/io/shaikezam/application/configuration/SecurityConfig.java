0d01c12f4d9063ddb9f6e294049b984b2869af9e
shaikezam <shaike.zam@gmail.com>
2023-06-19 22:47:14 +0300

    add spring security

---
+++ api-gateway/src/main/java/io/shaikezam/application/configuration/SecurityConfig.java
@@ -0,0 +1,20 @@
+package io.shaikezam.application.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SecurityConfig {
+    @Bean
+    public SecurityWebFilterChain springSecurityFilterChain ( ServerHttpSecurity http) {
+
+        http
+                .authorizeExchange()
+                .anyExchange()
+                .authenticated()
+                .and()
+                .oauth2Login(); // to redirect to oauth2 login page.
+
+        return http.build();
+    }
+}
